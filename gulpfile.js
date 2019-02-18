77c699bc8cefbd13a2bf9832b0493758bc2936c7
Alex Hudson <alex_hudson@apple.com>
2019-02-18 11:27:10 -0800

    Initialized handlebars templating and added corp data for all signatures

--- gulpfile.js
+++ gulpfile.js
@@ -1,2 +1,19 @@
-const gulp = require('gulp'),
-	  hbs = require('handlebars');
+const data       = require('./data/corp-data.json'),
+      gulp       = require('gulp'),
+	  compilehbs = require('gulp-compile-handlebars'),
+	  rename     = require('gulp-rename');
+
+
+gulp.task('hbs', function(){
+    data.employees.forEach(function(employee){
+        gulp.src('./template/signature.hbs')
+            .pipe(compilehbs(employee, {
+                    ignorePartials: true
+                }))
+            .pipe(rename(function (path) {
+                path.basename = employee.filename;
+                path.extname = ".html";
+            }))
+            .pipe(gulp.dest('./signatures'));
+    });
+});
